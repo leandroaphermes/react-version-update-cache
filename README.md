f4fcfcc2d56b360cfec4b0443c7f0d3b83f05368
Leandro Hermes <leandro.aparecido.hermes@gmail.com>
2022-02-23 13:23:14 -0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # react-version-update-cache
+
+Validate application version and notify you if you have a new version sent
+
+### Configs
+
+| Property          | Type   | Description                  | Default    |
+| ----------------- | ------ | ---------------------------- | ---------- |
+| storageKeyVersion | string | Current app version variable | required   |
+| duration          | number | Check update time interval   | 60 seconds |
+| basePath          | string | Endpoint exist to filename   | /          |
+| filename          | string | Name file to endpoint        | meta.json  |
+
+### Props Return
+
+| Property          | Type     | Description                                    |
+| ----------------- | -------- | ---------------------------------------------- |
+| islatestVersion   | boolean  | Return `true` or `false` if app latest version |
+| latestVersion     | string   | Current app version                            |
+| loading           | boolean  | Update status                                  |
+| emptyCacheStorage | function | Click to start update                          |
+
+### Example Implementation based hook
+
+```jsx
+import { useVersionUpdateCache } from "react-version-update-cache";
+
+function App() {
+  const { isLatestVersion, emptyCacheStorage, latestVersion } =
+    useVersionUpdateCache({
+      duration: 300 * 1000,
+      storageKeyVersion: process.env.REACT_APP_VERSION,
+    });
+
+  return (
+    <div>
+      <h4>My App component</h4>
+      {!isLatestVersion && (
+        <div>
+          <p>New version available</p>
+          <button onClick={() => emptyCacheStorage()}>Update Now</button>
+        </div>
+      )}
+    </div>
+  );
+}
+```
+
+### Example Implementation based Context
+
+```jsx
+import VersionUpdateCacheProvider from "react-version-update-cache";
+
+function App() {
+  const { isLatestVersion, emptyCacheStorage } = useVersionUpdateCache({
+    duration: 300 * 1000,
+    storageKeyVersion: process.env.REACT_APP_VERSION,
+  });
+
+  return (
+    <div>
+      <h4>My App component</h4>
+      <VersionUpdateCacheProvider>
+        {({ isLatestVersion, emptyCacheStorage }) => {
+          return !isLatestVersion ? (
+            <div>
+              <p>New version available</p>
+              <button onClick={() => emptyCacheStorage()}>Update Now</button>
+            </div>
+          ) : (
+            <LayoutAppRoutes />
+          );
+        }}
+        }
+      </VersionUpdateCacheProvider>
+    </div>
+  );
+}
+```
--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,52 @@
-import * as React from 'react';
+import * as React from "react";
 
 const defaultProps = {
   duration: 60 * 1000,
   auto: false,
-  basePath: '',
-  filename: 'meta.json',
+  basePath: "",
+  filename: "meta.json",
 };
 
 type Result = {
-  loading: boolean,
-  isLatestVersion: boolean,
-  emptyCacheStorage: (version?:string | undefined) => Promise<void>
-}
-
-type ProviderProps = {
-  duration?: number,
-  auto?: boolean,
-  storageKeyVersion: string,
-  basePath?: string,
-  filename?: string,
-  children?: any,
+  loading: boolean;
+  isLatestVersion: boolean;
+  latestVersion: string;
+  emptyCacheStorage: (version?: string | undefined) => Promise<void>;
 };
 
+type ProviderProps = {
+  duration?: number;
+  auto?: boolean;
+  storageKeyVersion: string;
+  basePath?: string;
+  filename?: string;
+  children?: any;
+};
 
-const VersionUpdateCacheContext = React.createContext({ } as Result)
+const VersionUpdateCacheContext = React.createContext({} as Result);
 
-export const useVersionUpdateCacheCtx = () => React.useContext(VersionUpdateCacheContext)
-
+export const useVersionUpdateCacheCtx = () =>
+  React.useContext(VersionUpdateCacheContext);
 
 export const VersionUpdateCacheProvider: React.FC<ProviderProps> = (props) => {
-  const { children, ...otherProps } = props
-  const result = useVersionUpdateCache(otherProps)
+  const { children, ...otherProps } = props;
+  const result = useVersionUpdateCache(otherProps);
   return (
-    <VersionUpdateCacheContext.Provider value={result} >
+    <VersionUpdateCacheContext.Provider value={result}>
       {children}
     </VersionUpdateCacheContext.Provider>
-  )
-}
-
+  );
+};
 
 export const useVersionUpdateCache = (props: ProviderProps) => {
-  const { duration, auto, storageKeyVersion, basePath, filename } = React.useMemo<ProviderProps>( () => ({
-    ...defaultProps,
-    ...props
-  }), [ defaultProps, props ]);
+  const { duration, auto, storageKeyVersion, basePath, filename } =
+    React.useMemo<ProviderProps>(
+      () => ({
+        ...defaultProps,
+        ...props,
+      }),
+      [defaultProps, props]
+    );
 
   const [loading, setLoading] = React.useState(true);
   const [appVersion, setAppVersion] = React.useState(storageKeyVersion);
@@ -51,12 +54,14 @@ export const useVersionUpdateCache = (props: ProviderProps) => {
   const [latestVersion, setLatestVersion] = React.useState(appVersion);
 
   const emptyCacheStorage = async (version?: string) => {
-    if ('caches' in window) {
+    if ("caches" in window) {
       // Service worker cache should be cleared with caches.delete()
       const cacheKeys = await window.caches.keys();
-      await Promise.all(cacheKeys.map(key => {
-        window.caches.delete(key)
-      }));
+      await Promise.all(
+        cacheKeys.map((key) => {
+          window.caches.delete(key);
+        })
+      );
     }
 
     // clear browser cache and reload page
@@ -65,15 +70,15 @@ export const useVersionUpdateCache = (props: ProviderProps) => {
   };
 
   // Replace any last slash with an empty space
-  const baseUrl = basePath?.replace(/\/+$/, '') + '/' + filename;
+  const baseUrl = basePath?.replace(/\/+$/, "") + "/" + filename;
 
   const fetchMeta = React.useCallback(() => {
     try {
       fetch(baseUrl, {
-        cache: 'no-store'
+        cache: "no-store",
       })
-        .then(response => response.json())
-        .then(meta => {
+        .then((response) => response.json())
+        .then((meta) => {
           const newVersion = meta.version;
           const isUpdated = newVersion === appVersion;
           if (!isUpdated && !auto) {
@@ -94,51 +99,52 @@ export const useVersionUpdateCache = (props: ProviderProps) => {
     } catch (err) {
       console.error(err);
     }
-  }, [ appVersion, auto ])
+  }, [appVersion, auto]);
 
   React.useEffect(() => {
-
-    let refinterval : any = undefined
+    let refinterval: any = undefined;
 
     const startCheckInterval = () => {
       if (window.navigator.onLine) {
         refinterval = setInterval(() => fetchMeta(), duration);
       }
-    }
+    };
 
     const stopCheckInterval = () => {
       clearInterval(refinterval);
-    }
+    };
 
-    window.addEventListener('focus', startCheckInterval);
-    window.addEventListener('blur', stopCheckInterval);
+    window.addEventListener("focus", startCheckInterval);
+    window.addEventListener("blur", stopCheckInterval);
     () => {
-      window.removeEventListener('focus', startCheckInterval);
-      window.removeEventListener('blur', stopCheckInterval);
+      window.removeEventListener("focus", startCheckInterval);
+      window.removeEventListener("blur", stopCheckInterval);
     };
   }, []);
 
   React.useEffect(() => {
     fetchMeta();
-  }, [ fetchMeta ]);
+  }, [fetchMeta]);
 
   return {
     loading,
     isLatestVersion,
     emptyCacheStorage,
-    latestVersion
+    latestVersion,
   };
 };
 
-const VersionUpdateCache: React.FC<ProviderProps> = props => {
-  const { loading, isLatestVersion, emptyCacheStorage } = useVersionUpdateCache(props);
+const VersionUpdateCache: React.FC<ProviderProps> = (props) => {
+  const { loading, isLatestVersion, emptyCacheStorage, latestVersion } =
+    useVersionUpdateCache(props);
 
   const { children } = props;
 
   return children({
     loading,
     isLatestVersion,
-    emptyCacheStorage
+    latestVersion,
+    emptyCacheStorage,
   });
 };
 
